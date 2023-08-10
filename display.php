6f9264c368fe55dfe70b70093286203b0d7c9d3c
galupo1 <alupograce22@gmail.com>
2023-08-10 13:16:28 +0300

    updating data file

--- display.php
+++ display.php
@@ -6,8 +6,9 @@ require_once "dbconnect.php";
 $query = "SELECT*FROM registration";
        
   $result = mysqli_query($dbconnect, $query);
-  while($data=mysqli_fetch_assoc(result)){
-    echo "This is the firstname" .$data['fistname']. "<br />"
+  while($data=mysqli_fetch_assoc(result))
+  {
+    echo "This is the firstname" .$data['fistname']. "<br />";
 }
 
 ?>
